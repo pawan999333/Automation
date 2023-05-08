ffce603b1961eb3a086bce1a7c8dead0f41b90a2
Pawan Kumar Sharma <pawansharma19feb@gmail.com>
2023-05-09 00:34:24 +0530

    Add files via upload

---
+++ Selenium Automation/lec-18/ClassExamples (2)/ClassExamples/LocatorsDemo1.java
@@ -0,0 +1,35 @@
+package day18;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LocatorsDemo1 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		//Open app
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize(); // maximize browser window
+		
+		//search box
+		driver.findElement(By.id("search_query_top")).sendKeys("T-shirts");
+		
+		//search button
+		driver.findElement(By.name("submit_search")).click();
+		
+		//link text & partial linktext
+		//driver.findElement(By.linkText("Printed Chiffon Dress")).click();
+		driver.findElement(By.partialLinkText("Printed Chiffon")).click();
+		
+		
+		
+		
+	}
+
+}
